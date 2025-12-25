1b5d5f44e6216aeece78ca6591f5bd5c6b521905
TaleemHossain <taleemhossain2506@gmail.com>
2025-12-26 02:09:17 +0530

    gravity and jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,11 +3,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [Header("Ground Check")]
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Vector2 groundCheckOffset = Vector2.zero;
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
     private CapsuleCollider2D capsuleCollider;
-    [SerializeField] private float speed = 5f;
     float horizontal;
+    bool isGrounded = false;
 
     void Start()
     {
@@ -17,7 +24,23 @@ public class PlayerMovement : MonoBehaviour
     }
     void Update()
     {
+        isGrounded = GroundCheck();
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+    bool GroundCheck()
+    {
+        Vector2 position = new(transform.position.x + groundCheckOffset.x, transform.position.y + groundCheckOffset.y);
+        Collider2D collider = Physics2D.OverlapCircle(position, groundCheckRadius, groundLayer);
+        return collider != null;
+    }
+    void OnDrawGizmos()
+    {
+        Vector2 position = new(transform.position.x + groundCheckOffset.x, transform.position.y + groundCheckOffset.y);
+        if (isGrounded)
+            Gizmos.color = Color.green;
+        else
+            Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(position, groundCheckRadius);
     }
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -27,11 +50,20 @@ public class PlayerMovement : MonoBehaviour
         {
             spriteRenderer.flipX = true;
             capsuleCollider.offset = new Vector2(Mathf.Abs(capsuleCollider.offset.x), capsuleCollider.offset.y);
+            groundCheckOffset.x = Mathf.Abs(groundCheckOffset.x);
         }
         else if (horizontal > 0)
         {
             spriteRenderer.flipX = false;
             capsuleCollider.offset = new Vector2(-Mathf.Abs(capsuleCollider.offset.x), capsuleCollider.offset.y);
+            groundCheckOffset.x = -Mathf.Abs(groundCheckOffset.x);
+        }
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 }

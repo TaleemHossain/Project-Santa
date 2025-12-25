f82e642525f92ff95fb269755648b99294e6e8ad
TaleemHossain <taleemhossain2506@gmail.com>
2025-12-25 23:00:19 +0530

    movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private CapsuleCollider2D capsuleCollider;
+    [SerializeField] private float speed = 5f;
+    float horizontal;
+
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody2D>();
+        spriteRenderer = transform.GetComponent<SpriteRenderer>();
+        capsuleCollider = transform.GetComponent<CapsuleCollider2D>();
+    }
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 inputVector = context.ReadValue<Vector2>().normalized;
+        horizontal = inputVector.x;
+        if (horizontal < 0)
+        {
+            spriteRenderer.flipX = true;
+            capsuleCollider.offset = new Vector2(Mathf.Abs(capsuleCollider.offset.x), capsuleCollider.offset.y);
+        }
+        else if (horizontal > 0)
+        {
+            spriteRenderer.flipX = false;
+            capsuleCollider.offset = new Vector2(-Mathf.Abs(capsuleCollider.offset.x), capsuleCollider.offset.y);
+        }
+    }
+}

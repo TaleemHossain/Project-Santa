bbe99274f6c73f3dd2c899772989dd1fbf1e49c1
TaleemHossain <taleemhossain2506@gmail.com>
2026-01-01 14:37:14 +0530

    video ui

--- Assets/Scripts/Mainmenu.cs
+++ Assets/Scripts/Mainmenu.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class Mainmenu : MonoBehaviour
 {
     [SerializeField] Button[] levels;
     [SerializeField] GameObject PausePanel;
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private GameObject videoCanvas;
     bool isPaused;
     void Awake()
     {
@@ -30,15 +33,34 @@ public class Mainmenu : MonoBehaviour
     }
     public void LoadLevel(int i)
     {
-        if(isPaused)
+        if (isPaused)
         {
             PauseMenu();
         }
         SceneManager.LoadSceneAsync(i);
     }
+    public void PlayLevel1WithIntro()
+    {
+        foreach (Button b in levels)
+            b.interactable = false;
+
+        videoCanvas.SetActive(true);
+        videoPlayer.Play();
+
+        videoPlayer.loopPointReached += OnIntroFinished;
+    }
+    void OnIntroFinished(VideoPlayer vp)
+    {
+        videoPlayer.loopPointReached -= OnIntroFinished;
+
+        videoCanvas.SetActive(false);
+
+        SceneManager.LoadSceneAsync(2);
+    }
+
     public void ReloadLevel()
     {
-        if(isPaused)
+        if (isPaused)
         {
             PauseMenu();
         }
@@ -50,7 +72,7 @@ public class Mainmenu : MonoBehaviour
         PlayerItems playerItems = FindAnyObjectByType<PlayerItems>();
         if (playerItems.GetNextLevel())
         {
-            if(PlayerPrefs.GetInt("HighestUnlocked") <= SceneManager.GetActiveScene().buildIndex - 1)
+            if (PlayerPrefs.GetInt("HighestUnlocked") <= SceneManager.GetActiveScene().buildIndex - 1)
             {
                 PlayerPrefs.SetInt("HighestUnlocked", SceneManager.GetActiveScene().buildIndex);
                 Debug.Log("Set to " + SceneManager.GetActiveScene().buildIndex);
@@ -62,13 +84,13 @@ public class Mainmenu : MonoBehaviour
 
     public void PauseMenu()
     {
-        if(PausePanel == null) return;
-        if(!isPaused) 
+        if (PausePanel == null) return;
+        if (!isPaused)
         {
             Time.timeScale = 0f;
             PausePanel.SetActive(true);
             isPaused = true;
-        } 
+        }
         else
         {
             PausePanel.SetActive(false);
